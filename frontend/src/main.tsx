e7f8f6a96fde4c89207cdb4890775b07e790a666
Jakub <jakub2002t@gmail.com>
2025-10-23 00:03:59 +0200

    frontend: React configuration, main and font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "@fontsource/roboto/300.css"; // Light
+import "@fontsource/roboto/400.css"; // Regular
+import "@fontsource/roboto/500.css"; // Medium
+import "@fontsource/roboto/700.css"; // Bold
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "@/theme";
+import App from "./App.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 5 * 60 * 1000, refetchOnWindowFocus: false },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
---
+++ frontend/src/theme/index.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  //   palette,
+  //   typography,
+  shape: { borderRadius: 8 },
+  spacing: 8,
+});
+
+export default theme;
